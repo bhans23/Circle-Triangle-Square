458404620ffba5cf314a26e675b5c85499b93f95
Ben <bhans23@gmail.com>
2020-07-15 18:23:09 -0600

    Game board updates

--- src/scenes/Level1.js
+++ src/scenes/Level1.js
@@ -1,17 +1,26 @@
-import {Scene} from 'phaser';
-import L1Logo from '../assets/level1.png';
+import { Scene } from "phaser";
 
 
 export default class Level1 extends Scene {
-    constructor() {
-        super('level1')
-    }
+  constructor() {
+    super("level1");
+  }
 
-    preload() {
-        this.load.image('L1Logo', L1Logo);
-    }
+  preload() {
+   
+    // load the tilemap
+    this.load.tilemapTiledJSON('level1GameBoard','assets/level1.json')
+    //load the sprite sheet
+    this.load.spritesheet('tiles',"assets/tiles.png",{frameWidth: 200, frameHeight: 200});
+  }
 
-    create() {
-        this.add.image(0,0,'L1Logo').setOrigin(0,0);
-    }
+  create() {
+    // this.add.image(400, 300, "level1Image", Level1Image);
+    // //create the tilemap
+    const board = this.make.tilemap({ key: 'level1GameBoard' });
+    //add tileset image
+    const tilePNG = this.board.addTilesetImage('tiles');
+    // create our layers
+    const backGroundLayer = this.board.createStaticLayer('background', tilesPNG,0,0);
+  }
 }
