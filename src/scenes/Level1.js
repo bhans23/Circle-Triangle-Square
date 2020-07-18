0a04f68e5e41b4f81f1130e9dd285c73911caa93
Ben <bhans23@gmail.com>
2020-07-18 08:13:26 -0600

    Test

--- src/scenes/Level1.js
+++ src/scenes/Level1.js
@@ -12,7 +12,62 @@ export default class Level1 extends Scene {
     this.createMap();
   }
   resize(width, height) {
-    if (width === undefined) {
+    if (width === undefined) {import { Scene } from "phaser";
+
+    export default class Level1 extends Scene {
+      constructor() {
+        super("level1");
+      }
+    
+      preload() {}
+    
+      create() {
+        this.events.on("resize", this.resize, this);
+        this.createMap();
+        //circle sprite
+        this.circleSprite = this.physics.add.sprite(500, 900, "circle");
+        this.circleMoves();
+      }
+    
+      update() {}
+    
+      resize(width, height) {
+        if (width === undefined) {
+          width = this.sys.game.config.width;
+          height = this.sys.game.config.height;
+        }
+        this.cameras.resize(width, height);
+      }
+      createMap() {
+        // //create the tilemap
+        const board = this.make.tilemap({ key: "level1GameBoard" });
+        //add tileset image
+        const tilesPNG = board.addTilesetImage("tiles");
+        // create our layers
+        const backGroundLayer = board.createStaticLayer(
+          "background",
+          tilesPNG,
+          0,
+          0
+        );
+        // backGroundLayer.setDepth(0);
+        const topLayer = board.createStaticLayer("top", tilesPNG, 0, 0);
+        // topLayer.setDepth(1);
+      }
+    
+      circleMoves() {
+        this.pointerCord = game.input.mousePointer;
+        this.pointer = this.input.activePointer;
+        this.input.on("pointerdown", function (pointer) {
+          this.physics.moveTo(this.circleSprite,this.pointerCord.x,this.pointerCord.y, 240);
+        }, this);
+        if (this.circleSprite.x == this.pointerCord.x && this.circleSprite.y == this.pointerCord.y ) {
+          this.circleSprite.stop();
+        }
+        
+      }
+    }
+    
       width = this.sys.game.config.width;
       height = this.sys.game.config.height;
     }
--- src/scenes/bootScene.js
+++ src/scenes/bootScene.js
@@ -4,6 +4,7 @@ import gameBoard from "../assets/level1.json";
 import logo from "../assets/sQTLogo.png";
 import startButton from "../assets/startButton.png";
 import mountainBG from "../assets/mountainBG.png";
+import circleSprite from "../assets/Circle.png";
 
 export default class bootScene extends Scene {
   constructor() {
@@ -18,6 +19,7 @@ export default class bootScene extends Scene {
     this.load.image("logo", logo);
     //level 1 assets
     this.load.tilemapTiledJSON("level1GameBoard", gameBoard);
+    this.load.image("circle", circleSprite);
     this.load.spritesheet("tiles", tilePNG, {
       frameWidth: 200,
       frameHeight: 200,
