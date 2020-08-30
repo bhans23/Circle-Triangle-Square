f7d067612ce460515ff8db4ea81ab2129a50b7b0
Ben <bhans23@gmail.com>
2020-08-30 15:20:05 -0600

    added pillars, and movements

--- src/logic/CircleSprite.js
+++ src/logic/CircleSprite.js
@@ -5,11 +5,31 @@ export default class CircleSprite extends spriteCreation {
   constructor(spriteValues) {
     super(spriteValues);
     this.gB = spriteValues.gB;
+    this.graphics = this.scene.add.graphics().setDepth(1);
+    this.speed = null;
   }
 
-  moves() {
-    this.selectedSquare = this.gB.sqNum.indexOf(this.gB.sqNum[this.scene.sqI]);
+  moves(x, y) {
+    //filter out available moves and border
+    this.selectedSquare = this.gB.sqNum.indexOf(this.gB.sqNum[this.scene.spSq]);
+    //Removing border squares
     this.availableMoves = this.gB.sqIndex.filter(
+      (x) => x < this.gB.sqIndex.length - this.gB.rows
+    );
+    this.availableMoves = this.availableMoves.filter((x) => x > this.gB.rows);
+    for (let i = 0; i < this.gB.sqIndex.length; i = i + this.gB.rows) {
+      this.availableMoves = this.availableMoves.filter(
+        (x) => x !== this.gB.sqIndex[i]
+      );
+    }
+    for (let i = -1; i < this.gB.sqIndex.length; i = i + this.gB.rows) {
+      this.availableMoves = this.availableMoves.filter(
+        (x) => x !== this.gB.sqIndex[i]
+      );
+    }
+    
+    // Filtering out only sprite specific moves squares
+    this.availableMoves = this.availableMoves.filter(
       (x) =>
         x === this.selectedSquare - 1 ||
         x === this.selectedSquare + 1 ||
@@ -20,14 +40,11 @@ export default class CircleSprite extends spriteCreation {
     this.nonAvailMoves = this.gB.sqIndex.filter(
       (x) => !this.availableMoves.includes(x)
     );
-    if (this.body.speed > 0) {
-      
-      this.graphics.clear();
-    } else {
-      this.graphics.fillStyle(0xffff00, 0.2);
-      this.availableMoves.map((x) =>
-        this.graphics.fillRectShape(this.gB.sqNum[x])
-      );
-    }
+    // Add highlights to squares
+    this.graphics.clear();
+    this.graphics.fillStyle(0x2f40de, 0.6);
+    this.availableMoves.map((x) =>
+      this.graphics.fillRectShape(this.gB.sqNum[x])
+    );
   }
 }
--- src/scenes/Level1.js
+++ src/scenes/Level1.js
@@ -2,6 +2,7 @@ import { Scene } from "phaser";
 import SquareSprite from "../logic/SquareSprite";
 import CircleSprite from "../logic/CircleSprite";
 import TriangleSprite from "../logic/TriangleSprite";
+import Pillar from "../logic/Pillar";
 import GameBoard from "../logic/GameBoard";
 
 export default class Level1 extends Scene {
@@ -16,35 +17,18 @@ export default class Level1 extends Scene {
     // this.higlightSquares()
     this.events.on("resize", this.resize, this);
     this.createMap();
-    //sprite
-    this.spriteSelection = [
-      new SquareSprite({
-        scene: this,
-        x: 500,
-        y: 900,
-        key: "square",
-        gB: this.gB,
-      }).setDepth(2),
-      // new CircleSprite({
-      //   scene: this,
-      //   x: 300,
-      //   y: 900,
-      //   key: "circle",
-      //   gB: this.gB,
-      // }).setDepth(2),
-      // new TriangleSprite({
-      //   scene: this,
-      //   x: 700,
-      //   y: 900,
-      //   key: "triangle",
-      //   gB: this.gB,
-      // }).setDepth(2),
-    ];
+    this.createSprites();
+    this.createGameObjects();
     this.spriteMoveTo();
+    
   }
 
   update() {
     this.spriteMoves();
+    this.addCollisions();
+    
+
+    // this.pillarMoves();
   }
 
   //--Sprite Move functions----------------------------------------------------
@@ -94,6 +78,7 @@ export default class Level1 extends Scene {
         let sqX = Math.floor(pointer.x / this.gB.sqW);
         let sqY = Math.floor(pointer.y / this.gB.sqH);
         this.sqI = this.gB.squareMatrix[sqX][sqY];
+
         sprite.target.x = this.gB.sqNum[this.sqI].x + this.gB.sqW / 2;
         sprite.target.y = this.gB.sqNum[this.sqI].y + this.gB.sqH / 2;
 
@@ -107,6 +92,11 @@ export default class Level1 extends Scene {
 
   spriteMoves() {
     this.spriteSelection.forEach((sprite) => {
+      sprite.update();
+    });
+  }
+  pillarMoves() {
+    this.pillars.forEach((sprite) => {
       sprite.update();
     });
   }
@@ -132,14 +122,14 @@ export default class Level1 extends Scene {
     this.gB.squareBoard();
     this.gB.sqNum.map((x) => x.setDepth(10));
 
-    for (let i = 0; i < this.gB.sqNum.length; i++) {
-      this.add
-        .text(this.gB.sqNum[i].x, this.gB.sqNum[i].y, i, {
-          color: "#ffffff",
-          fontSize: "30px",
-        })
-        .setDepth(1);
-    }
+    // for (let i = 0; i < this.gB.sqNum.length; i++) {
+    //   this.add
+    //     .text(this.gB.sqNum[i].x, this.gB.sqNum[i].y, i, {
+    //       color: "#ffffff",
+    //       fontSize: "30px",
+    //     })
+    //     .setDepth(1);
+    // }
   }
 
   //--------------------------------------------------------------------
@@ -150,6 +140,25 @@ export default class Level1 extends Scene {
     }
     this.cameras.resize(width, height);
   }
+  addCollisions() {
+    this.physics.add.collider(
+      this.spriteSelection,
+      this.pillars,
+      (sprite, pillar2) => {
+        this.pillars.forEach((pillar) => {
+          if (pillar === pillar2) {
+            pillar.setImmovable(false);
+          } else {
+            pillar.setImmovable(true);
+          }
+        });
+      }
+    );
+
+    this.physics.add.collider(this.pillars, this.pillars);
+    this.physics.add.collider(this.spriteSelection, this.topLayer);
+    this.physics.add.collider(this.pillars, this.topLayer);
+  }
 
   createMap() {
     // //create the tilemap
@@ -157,12 +166,73 @@ export default class Level1 extends Scene {
     //add tileset image
     const tilesPNG = board.addTilesetImage("tiles");
     // create our layers
-    const backGroundLayer = board.createStaticLayer(
-      "background",
-      tilesPNG,
-      0,
-      0
-    );
-    const topLayer = board.createStaticLayer("top", tilesPNG, 0, 0);
+    this.backGroundLayer = board.createStaticLayer("floor", tilesPNG, 0, 0);
+    this.topLayer = board.createStaticLayer("wall", tilesPNG, 0, 0);
+    this.topLayer.setCollisionByExclusion([-1]);
+    
+  }
+  createSprites() {
+    this.spriteSelection = [
+      // new SquareSprite({
+      //   scene: this,
+      //   x: 500,
+      //   y: 900,
+      //   key: "square",
+      //   gB: this.gB,
+      // }).setDepth(2),
+      new CircleSprite({
+        scene: this,
+        x: 300,
+        y: 900,
+        key: "circle",
+        gB: this.gB,
+      })
+        .setDepth(2)
+        .setBounce(0),
+        
+      // new TriangleSprite({
+      //   scene: this,
+      //   x: 700,
+      //   y: 900,
+      //   key: "triangle",
+      //   gB: this.gB,
+      // }).setDepth(2),
+    ];
+  }
+  createGameObjects() {
+    this.pillars = [
+      new Pillar({
+        scene: this,
+        x: 500,
+        y: 500,
+        key: "pillar",
+        gB: this.gB,
+        selected: this.selectedSquare,
+      })
+        .setDepth(2),
+        
+
+      new Pillar({
+        scene: this,
+        x: 300,
+        y: 300,
+        key: "pillar",
+        gB: this.gB,
+        selected: this.selectedSquare,
+      })
+        .setDepth(2),
+        
+
+      new Pillar({
+        scene: this,
+        x: 700,
+        y: 300,
+        key: "pillar",
+        gB: this.gB,
+        selected: this.selectedSquare,
+      })
+        .setDepth(2),
+        
+    ];
   }
 }
